33e60ea3ef7ebcf34b6ea0b4e119d7608364ab4a
gaebabja <hippo420@naver.com>
2024-09-01 21:37:57 +0900

    최초커밋

---
+++ src/test/java/app/mohe/MoheApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mohe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
